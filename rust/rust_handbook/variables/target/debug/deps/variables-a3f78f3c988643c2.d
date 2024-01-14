3a67862e3dc7cf21fb10b37458772f2be25da524
Lauric <62937013+Lauric-h@users.noreply.github.com>
2024-01-14 13:35:59 +0100

    Add Rust variables

---
+++ rust/rust_handbook/variables/target/debug/deps/variables-a3f78f3c988643c2.d
@@ -0,0 +1,5 @@
+/Users/LAURIC/dev/perso/learnings/rust/rust_handbook/variables/target/debug/deps/variables-a3f78f3c988643c2: src/main.rs
+
+/Users/LAURIC/dev/perso/learnings/rust/rust_handbook/variables/target/debug/deps/variables-a3f78f3c988643c2.d: src/main.rs
+
+src/main.rs:
---
+++ rust/rust_handbook/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/LAURIC/dev/perso/learnings/rust/rust_handbook/variables/target/debug/variables: /Users/LAURIC/dev/perso/learnings/rust/rust_handbook/variables/src/main.rs
