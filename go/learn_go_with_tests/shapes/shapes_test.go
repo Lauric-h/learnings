17c980fb9522b0b0732f8be2522be9516c96c62a
Lauric-h <h.lauric@outlook.com>
2025-04-16 16:42:45 +0200

    structs pointers

---
+++ go/learn_go_with_tests/shapes/shapes_test.go
@@ -0,0 +1,31 @@
+package shapes
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 10.0, Height: 10.0}, hasArea: 100.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+		}
+	}
+}
